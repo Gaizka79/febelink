d1d0c81d8348f4dd2423895b7ddb70c78d9f053d
Gaizka79 <agaizka@hotmail.com>
2023-03-29 21:22:31 +0200

    helmet

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const helmet = require("helmet");
 const app = express();
 const cors = require("cors");
 
 const routes = require('./server/routes/routes.js');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
+app.use(helmet());
 app.use(cors());
 
 app.use(express.json());
